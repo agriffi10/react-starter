e71375862c30ddc9e08346a1182192077cbc50e5
agriffi10 <agriffi10@gmail.com>
2022-03-12 14:15:58 -0500

    Added in browser router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
-import React from "react";
-import logo from "./logo.svg";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
 import "./App.css";
 
 function App() {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
